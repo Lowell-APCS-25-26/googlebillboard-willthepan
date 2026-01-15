f9c86ec9330be16571e6f77fe849e457746789ff
willthepan <williampan.us@gmail.com>
2026-01-15 17:38:42 +0000

    added isPrime

--- lib/src/main/java/org/APCSLowell/GoogleBillboard.java
+++ lib/src/main/java/org/APCSLowell/GoogleBillboard.java
@@ -5,8 +5,16 @@ package org.APCSLowell;
 
 public class GoogleBillboard {
     public boolean isPrime(long n) {
-        // Copy and paste your answer from CodingBat appropriatley here.
-        return false;
+        if(n<2){
+  return false;
+}
+  for(int i=2; i<= Math.sqrt(n); i++){
+    if(n%i == 0){
+      return false;
+    }
+      
+  }
+  return true;
     }
     public long firstPrimeNumberIn(String n) {
         // Write your code here.
